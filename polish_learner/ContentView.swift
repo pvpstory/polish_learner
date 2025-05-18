e43c8be6f2f0b68a09d79c5a33abf9cf5443ef10
mikolaasinskij <koliyayyt@gmail.com>
2025-05-18 13:19:27 +0200

    Initial Commit

---
+++ polish_learner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  polish_learner
+//
+//  Created by Микола Ясінський on 18/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ polish_learner/polish_learnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  polish_learnerApp.swift
+//  polish_learner
+//
+//  Created by Микола Ясінський on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct polish_learnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
