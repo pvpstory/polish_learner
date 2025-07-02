52278d07569daf9b261966b47a3b418b69e6ff7c
mikolaasinskij <koliyayyt@gmail.com>
2025-07-02 12:43:48 +0200

    UI of the button

--- polish_learner/LearnPhase/MultiChoiceWord.swift
+++ polish_learner/LearnPhase/MultiChoiceWord.swift
@@ -17,28 +17,54 @@ struct MultiChoiceWord: View {
     let frontside: String
     let onAnwer: () -> Void
     @State var allOptions: [String]
+    @State var selectedAnswer: String?
     
-    
+    private let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 15),
+        GridItem(.flexible(), spacing: 15)
+    ]
     var body: some View{
         VStack{
-            HStack{
-                buttonAnswer(text: allOptions[0])
-                buttonAnswer(text: allOptions[1])
-            }
-            HStack{
-                buttonAnswer(text: allOptions[2])
-                buttonAnswer(text: allOptions[3])
+            LazyVGrid(columns: columns, spacing: 15){
+                ForEach(allOptions,id: \.self) { option in
+                buttonAnswer(text: option)}
             }
             
         }.task {
             allOptions.shuffle()
         }
     }
-}
-
-func buttonAnswer(text: String) ->  some View {
-    Button(text){
+    func buttonAnswer(text: String) ->  some View {
+        Button(action: {
+            if selectedAnswer == nil{
+                selectedAnswer = text
+            }
+        }){
+            Text(text).font(.title).foregroundStyle(.black)
+            
+        }.background(backgroundColorFor(option: text))
+    }
+    
+    func backgroundColorFor(option: String) -> Color{
+        if option == selectedAnswer && option != frontside{
+            return .red
+        }
+        else if option == frontside && option == selectedAnswer{
+            return .green
+        }
+        else{
+            if option == frontside && selectedAnswer != nil{
+                return .green.opacity(0.5)
+            }
+            return .white
+        }
         
     }
+
+}
+
+
+func buttonClicled(answer: String){
+    
 }
 
