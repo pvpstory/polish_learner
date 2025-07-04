5b36355ed6cd12a280bed329e5e554411f766a69
mikolaasinskij <koliyayyt@gmail.com>
2025-07-04 17:07:11 +0200

    fixing the changing of stages through creating a copy of the Flashcard object

--- polish_learner/LearnPhase/LearnPhaseMainView.swift
+++ polish_learner/LearnPhase/LearnPhaseMainView.swift
@@ -9,11 +9,18 @@ import Foundation
 import SwiftUI
 import SwiftData
 
+struct FlashcardCoppy{
+    var frontside: String
+    var backside: String
+    var stage: String
+}
+
 struct LearnPhaseMainView: View {
     @Query var learnPhaseFlashCards: [Flashcard]
     @State var flashcardsCoppy: [Flashcard] = []
     @State var currentIndex: Int = 0
     @State var canClickNext: Bool = false
+    @State var currentFlashCard: FlashcardCoppy = FlashcardCoppy(frontside: "", backside: "", stage: "")
     var body: some View {
         VStack{
             
@@ -32,12 +39,12 @@ struct LearnPhaseMainView: View {
                         Image(systemName: "arrow.right")
                     }.position(x: 1400, y: 380)
                 }
-                switch flashcardsCoppy[currentIndex].stage {
+                switch currentFlashCard.stage {
                 case "learning":
-                    MultiChoiceWord(backside: flashcardsCoppy[currentIndex].backside, frontside: flashcardsCoppy[currentIndex].frontside, onAnswer: onAnswer, allOptionsInput: ["tuka","buka","assasin",flashcardsCoppy[currentIndex].frontside])
+                    MultiChoiceWord(backside: currentFlashCard.backside, frontside: currentFlashCard.frontside, onAnswer: onAnswer, allOptionsInput: ["tuka","buka","assasin",currentFlashCard.frontside])
                 case "new":
-                    TypeTheWord(backside: flashcardsCoppy[currentIndex].backside, frontside:
-                                    flashcardsCoppy[currentIndex].frontside, onAnswer: onAnswer, TypedWord: "")
+                    TypeTheWord(backside: currentFlashCard.backside, frontside:
+                                    currentFlashCard.frontside, onAnswer: onAnswer, TypedWord: "")
                 case "reviewed":
                     Text("123123")
                 default:
@@ -56,22 +63,28 @@ struct LearnPhaseMainView: View {
     private func startNewSession(){
         flashcardsCoppy = Array(learnPhaseFlashCards)
         currentIndex = 0
-        
+        print("triggered")
+        currentFlashCard = FlashcardCoppy(frontside: flashcardsCoppy[currentIndex].frontside, backside: flashcardsCoppy[currentIndex].backside, stage: flashcardsCoppy[currentIndex].stage)
     }
     private func incrementIndex(){
+        
         if currentIndex < flashcardsCoppy.count-1{
             currentIndex+=1
             canClickNext = false
+            currentFlashCard = FlashcardCoppy(frontside: flashcardsCoppy[currentIndex].frontside, backside: flashcardsCoppy[currentIndex].backside, stage: flashcardsCoppy[currentIndex].stage)
         }
         else if currentIndex == flashcardsCoppy.count-1 {
             canClickNext = false
             currentIndex = 0
             flashcardsCoppy = []
         }
+        
     }
     private func onAnswer(isSuccess: Bool){
         canClickNext = true
-        //change the flashcard
+        if isSuccess{
+            flashcardsCoppy[currentIndex].stage = "learning"
+        }
         
     }
     
