50a5348590ea3e5a3e20cf466cf47ec5871a11aa
mikolaasinskij <koliyayyt@gmail.com>
2025-07-08 15:49:00 +0200

    getOptionWord implementation

--- polish_learner/ReviewPhase/ReviewPhaseMainView.swift
+++ polish_learner/ReviewPhase/ReviewPhaseMainView.swift
@@ -13,6 +13,7 @@ import SwiftData
 struct ReviewPhaseMainView: View {
     @Query(filter: #Predicate<Flashcard> { flashcard in
         flashcard.stage == "review"    }) var flashcards: [Flashcard] // change
+    @Query var randomFlashcards: [Flashcard] // is it inefficient?
     @State var testFormat: Int = Int.random(in: 0..<2)
     @State var flashcardsCoppy: [Flashcard] = []
     @State var curBackside: String = ""
@@ -63,24 +64,48 @@ struct ReviewPhaseMainView: View {
                 curFrontside = flashcardsCoppy[currentIndex].frontside
                 curBluredBackside = flashcards[currentIndex].backside_blured
             }
-            
-            
-            
         }
         
         
     }
     func incrementIndex() {
+        if currentIndex >= flashcardsCoppy.count - 1 {
+            whatToShow = "noCards"
+        }
+        else{
+            currentIndex += 1
+            canClickNext = false
+        }
+        
+        
         
     }
     func onAnwer(correct: Bool) {
         
+        canClickNext = true
+        
     }
     func getOptionWords() -> [String] {
-        var options: [String] = []
+        //make this func available to the LearnPhaseMainView
+        let shuffledFlashcards = randomFlashcards.shuffled() //is it inefficient?
+        var frontSides: [String] = []
+        for i in 0..<3{
+            if shuffledFlashcards.count >= i+1 && shuffledFlashcards[i].frontside != curFrontside{
+                frontSides.append(shuffledFlashcards[i].frontside)
+            }
+            else if shuffledFlashcards[i].frontside == curFrontside && shuffledFlashcards.count >= 5{
+                frontSides.append(shuffledFlashcards[4].frontside)
+            }
+            else{
+                frontSides.append("supeRandom")
+                
+            }
+        }
         
-        options.append(curFrontside)
-        return options
+
+        frontSides.append(curFrontside)
+        frontSides = frontSides.shuffled()
+        return frontSides
     }
 }
 
